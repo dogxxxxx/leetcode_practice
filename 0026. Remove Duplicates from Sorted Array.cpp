6d8e7a411c8546d94627ad2fd9b0bf35f90837ef
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-07-06 14:22:57 +0800

    Create 0026. Remove Duplicates from Sorted Array.cpp

---
+++ 0026. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,26 @@
+/*
+Method 1:
+Iterate the nums and keep previos element.
+If the current element is the same as previous one, erase it.
+
+Time complexity: O(N)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int tmp = nums[0];
+        int result = 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (tmp == nums[i]){
+                nums.erase(nums.begin() + i);
+                i--;
+            } else {
+                tmp = nums[i];
+                result ++;
+            }
+        }
+        return result;
+    }
+};

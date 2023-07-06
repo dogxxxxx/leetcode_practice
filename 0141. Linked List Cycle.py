643962beaefac3dc58d5b7ce821be5c738e6da12
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-07-06 10:51:04 +0800

    Update and rename 0141. Linked List Cycle.md to 0141. Linked List Cycle.py

--- 0141. Linked List Cycle.py
+++ 0141. Linked List Cycle.py
@@ -1,15 +1,16 @@
-```
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+"""
 Method 1:
 Keep the ID of each ListNode. If replicated, there is a cycle. Otherwise, there isn't.
 
 Time complexity: O(N)
 Space complexity: O(N)
+"""
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -24,13 +25,14 @@ class Solution:
             head = head.next
         return False
 
-
+"""
 Method 2:
 The maximum length of the ListNode is 10^4. Thus, if ListNode.next still exists after 10^4 iters, there's a cycle.
 This method doesn't work if the maximum length of the ListNode is not given.
 
 Time complexity: O(N)
 Space complexity: O(1)
+"""
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -41,13 +43,15 @@ class Solution:
             if i > 10000:
                 return True
         return False
-       
+
+"""
 Method 3:
 Create a pointer to loop through the ListNode with 2 steps per iteration, while the original head loops with 1 step.
 If the IDs of the pointer and the head are the same, there's a cycle.
 
 Time complexity: O(N)
 Space complexity: O(1)
+"""
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -58,4 +62,3 @@ class Solution:
             if id(head) == id(p1):
                 return True
         return False
-```

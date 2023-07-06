f81335b4a885fde649d2e01b0db2391db8ddd4fa
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-07-06 13:39:45 +0800

    Create 0027. Remove Element.cpp

---
+++ 0027. Remove Element.cpp
@@ -0,0 +1,22 @@
+/*
+Method 1:
+Loop over the nums list from the end. If the element in the list equals to val, erase it.
+
+Time complexity: O(N)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int nums_length = nums.size();
+        int erases = 0;
+        for (int i = nums_length - 1; i >= 0; i--){
+            if (nums[i] == val){
+                nums.erase(nums.begin()+i);
+                erases = erases + 1;
+            }
+        };
+        return nums_length - erases;
+    }
+};

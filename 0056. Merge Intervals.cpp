540f71a3a69b0851ade07235c06467094265d7f0
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-07-11 14:42:10 +0800

    Update 0056. Merge Intervals.cpp

--- 0056. Merge Intervals.cpp
+++ 0056. Merge Intervals.cpp
@@ -7,23 +7,28 @@ Time complexity: O(nlogn)
 Space complexity: O(N)
 */
 
-class Solution {
+class Solution 
+{
 public:
-    static bool sortcol(const vector<int>& v1, const vector<int>& v2){
-            return v1[0] < v2[0];
-        }
+    static bool sortcol(const vector<int>& v1, const vector<int>& v2)
+    {
+        return v1[0] < v2[0];
+    }
 
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& intervals)
+    {
         sort(intervals.begin(), intervals.end(), sortcol);
         vector<vector<int>> result;
 
-        for (int i = 1; i < intervals.size(); i++){
+        for (int i = 1; i < intervals.size(); i++)
+        {
             if (intervals[i-1][1] >= intervals[i][0])
             {
                 vector<int> newInterval = {intervals[i-1][0], max(intervals[i-1][1], intervals[i][1])};
                 intervals[i] = newInterval;
             }
-            else {
+            else
+            {
                 result.push_back(intervals[i-1]);
             }
         }

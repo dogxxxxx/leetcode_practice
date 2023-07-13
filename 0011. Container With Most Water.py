5a896fa83a5c4ea8913778b72a675a3a5b08a17a
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-07-13 12:11:07 +0800

    Update 0011. Container With Most Water.py

--- 0011. Container With Most Water.py
+++ 0011. Container With Most Water.py
@@ -24,3 +24,33 @@ class Solution:
             else:
                 h2 -= 1
         return result
+
+"""
+Method 2:
+Same as Method 1. Just skip area calculation for some situations.
+
+Time complexity: O(N)
+Space complexity: O(1)
+"""
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        def get_area(h1, h2, length):
+            area = min(h1, h2) * length
+            return area
+        
+        h1 = 0
+        h2 = len(height) - 1
+        result = 0
+        while h1 < h2:
+            area = get_area(height[h1], height[h2], h2-h1)
+            result = max(area, result)
+            if height[h1] <= height[h2]:
+                h1 += 1
+                while height[h1] <= height[h1-1] and h1 < h2:
+                    h1 += 1
+            else:
+                h2 -= 1
+                while height[h2] <= height[h2+1] and h1 < h2:
+                    h2 -= 1
+        return result

65c38eb862cf7e1a4c93cfa2fe42301f7849c2a4
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-10-12 16:09:45 +0800

    Update 0009. Palindrome Number.py

--- 0009. Palindrome Number.py
+++ 0009. Palindrome Number.py
@@ -20,3 +20,26 @@ class Solution:
                 if x[mid - i] != x[mid + i]:
                     return False
         return True
+
+"""
+Method 2:
+Get the reversed number of the last half part of x.
+
+Time complexity: O(N/2)
+Space complexity: O(1)
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x > 0 and x % 10 == 0):
+            return False
+
+        reverse = 0
+        while reverse < x:
+            reverse = reverse * 10 + x % 10
+            x = x // 10
+
+        if reverse == x or reverse // 10 == x:
+            return True
+        else:
+            return False

2cd3aa1efe6e47d3a83cc9692f449055f68d6632
dogxxxxx <53805780+dogxxxxx@users.noreply.github.com>
2023-12-14 17:09:14 +0800

    Create 1761. Minimum Degree of a Connected Trio in a Graph.py

---
+++ 1761. Minimum Degree of a Connected Trio in a Graph.py
@@ -0,0 +1,23 @@
+"""
+Method 1:
+Store degrees and connected edges of each points. Then, check if there's a trio.
+"""
+
+class Solution:
+    def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
+        result = 1000000
+
+        degrees = [0] * n
+        e = [set() for _ in range(n)]
+        for (a, b) in edges:
+            degrees[a - 1] += 1
+            degrees[b - 1] += 1
+            e[min(a, b) - 1].add(max(a,b))
+
+        for i in range(n):
+            for c in e[i]:
+                for tmp in e[c-1]:
+                    if tmp in e[i]:
+                        degree = degrees[i] + degrees[c - 1] + degrees[tmp - 1] - 6
+                        result = min(result, degree)
+        return result if result != 1000000 else -1

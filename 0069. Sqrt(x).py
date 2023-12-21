e0212141986f15f8ca2048f705f23c52b422f54b
daniel.chen <daniel.kh.chen@acercsi.com>
2023-12-21 11:51:07 +0800

    Create: 0069

---
+++ 0069. Sqrt(x).py
@@ -0,0 +1,24 @@
+"""
+Method 1:
+"""
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low = 1
+        high = x
+        while low <= high:
+            mid = low + (high - low) // 2
+            if mid == x / mid:
+                return mid
+            elif mid < x / mid:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    x = 2
+    result = sol.mySqrt(x)
+    print(result)

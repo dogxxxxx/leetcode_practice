372177a27fbddf440e648a2f632ad9c66c997c33
daniel.chen <daniel.kh.chen@acercsi.com>
2023-12-21 13:19:42 +0800

    Create: 0367

---
+++ 0367. Valid Perfect Square.py
@@ -0,0 +1,23 @@
+"""
+Method 1:
+"""
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low = 1
+        high = num
+        while low <= high:
+            mid = low + (high - low) // 2
+            if mid == num / mid:
+                return True
+            elif mid > num / mid:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+
+
+if __name__ == "__main__":
+    num = 9 
+    result = Solution().isPerfectSquare(num)
+    print(result)

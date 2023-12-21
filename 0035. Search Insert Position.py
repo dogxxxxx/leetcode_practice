77338e4012b2a87aa49d684d95340f239807f22b
daniel.chen <daniel.kh.chen@acercsi.com>
2023-12-21 11:23:42 +0800

    Create: 0035

---
+++ 0035. Search Insert Position.py
@@ -0,0 +1,30 @@
+from typing import List
+"""
+Method 1:
+Binary Search
+
+Time complexity: O(nlog(n))
+Space complexity: O(1)
+"""
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = int((low + high) / 2)
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] == target:
+                return mid
+            else:
+                high = mid - 1                
+        return low
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,3,5,6]
+    target = 2
+    result = sol.searchInsert(nums, target)
+    print(result)

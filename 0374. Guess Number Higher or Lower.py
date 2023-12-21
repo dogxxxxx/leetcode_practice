a3c6955347c3ea2c2d8d2331a45697f08c5942b8
daniel.chen <daniel.kh.chen@acercsi.com>
2023-12-21 13:44:16 +0800

    Create: 0374

---
+++ 0374. Guess Number Higher or Lower.py
@@ -0,0 +1,28 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+def guess(num: int) -> int:
+    pass
+"""
+Method 1:
+Binary Search
+
+Time complexity: O(nlog(n))
+Space complexity: O(1)
+"""
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        low = 1
+        high = n
+        while low <= high:
+            mid = low + (high - low) // 2
+            result = guess(mid)
+            if result == 0:
+                return mid
+            elif result == 1:
+                low = mid + 1
+            else:
+                high = mid - 1

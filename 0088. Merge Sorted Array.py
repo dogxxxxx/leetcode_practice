56b9ab75447503bb7f189e4c2b5b0701bee67c59
daniel.chen <daniel.kh.chen@acercsi.com>
2024-01-31 11:51:57 +0800

    Review: 0088

--- 0088. Merge Sorted Array.py
+++ 0088. Merge Sorted Array.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 Method 1:
 Recursive algorithm. Compare 2 numbers until nums2 list is all inserted into nums1 list.
@@ -31,7 +32,7 @@ class Solution:
 
 """
 Method 2:
-Simpl2 while loop.
+Simple while loop.
 
 Time complexity: O(m+n)
 Space complexity: O(M+n), M is the length of nums1
@@ -53,3 +54,35 @@ class Solution:
                 n = n - 1
             else:
                 m = m - 1
+
+"""
+Review 1:
+Add elements from the end
+until pointer 2 points to 0.
+
+Time complexity: O(m + n)
+Space complexity: O(1)
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        index = m + n - 1
+        for i in range(n - 1, -1, -1):
+            while m - 1 >= 0 and nums2[i] <= nums1[m - 1]:
+                nums1[index] = nums1[m - 1]
+                m -= 1
+                index -= 1
+            nums1[index] = nums2[i]
+            index -= 1
+
+
+if __name__ == "__main__":
+    nums1 = [2,0]
+    nums2 = [1]
+    n = 1
+    m = 1
+    Solution().merge(nums1, m, nums2, n)
+    print(nums1)

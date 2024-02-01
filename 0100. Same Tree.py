69cc791b83d00a2cbb3f9494b17ef3f9bbb989d8
daniel.chen <daniel.kh.chen@acercsi.com>
2024-02-01 10:02:19 +0800

    Add: method 2

--- 0100. Same Tree.py
+++ 0100. Same Tree.py
@@ -7,7 +7,10 @@ class TreeNode:
         self.right = right
 """
 Method 1:
+Use a variable to store result
 
+Time complexity: O(N)
+Space complexity: O(h)
 """
 
 class Solution:
@@ -28,4 +31,19 @@ class Solution:
         self.result = True
         self.compare(p, q)
         return self.result
+
+"""
+Method 2:
+Recursive
+"""
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        elif p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
     

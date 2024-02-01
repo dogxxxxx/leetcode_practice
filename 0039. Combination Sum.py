137b166b8fc879ac46471b602d7f6705d1265e47
daniel.chen <daniel.kh.chen@acercsi.com>
2024-02-01 15:46:53 +0800

    Review

--- 0039. Combination Sum.py
+++ 0039. Combination Sum.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 Method 1:
 DFS
@@ -19,3 +20,29 @@ class Solution:
                 dfs(candidates[i:], result_arr+[candidates[i]], current_sum+candidates[i])
         dfs(candidates, [], 0)
         return result
+
+"""
+Review 1:
+
+"""
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        def dfs(candidates, current_arr, current_sum):
+            if current_sum == target:
+                result.append(current_arr)
+                return
+            if current_sum > target:
+                return
+            for i in range(len(candidates)):
+                # current_arr.append(candidates[i])
+                # dfs(candidates[i:], current_arr, current_sum + candidates[i])
+                dfs(candidates[i:], current_arr + [candidates[i]], current_sum + candidates[i])
+        dfs(candidates, [], 0)
+        return result
+
+if __name__ == "__main__":
+    candidates = [2,3,6,7]
+    target = 7
+    result = Solution().combinationSum(candidates, target)
+    print(result)

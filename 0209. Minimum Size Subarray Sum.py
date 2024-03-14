ff1512662ed144def0d9d9c951ea3a6b3821e388
daniel.chen <daniel.kh.chen@acercsi.com>
2024-03-14 16:07:55 +0800

    Review

--- 0209. Minimum Size Subarray Sum.py
+++ 0209. Minimum Size Subarray Sum.py
@@ -1,3 +1,4 @@
+from typing import List
 """
 Method 1:
 Use 2 pointers and calculate sum of elements between these pointers.
@@ -5,8 +6,8 @@ If sum >= target, move left pointer one unit to the right.
 Otherwise, move right pointer one unit to the right until right pointer points
 to the end of nums.
 
-Time complexity:
-Space complexity:
+Time complexity: O(N)
+Space complexity: O(1)
 """
 
 class Solution:
@@ -28,3 +29,32 @@ class Solution:
         if result == len(nums) + 1:
             return 0
         return result
+    
+"""
+Method 2:
+Use sliding window to calculate current sum
+
+Time complexity: O(N)
+Space complexity: O(1)
+Date: 2024/03/14
+"""
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        p1 = 0
+        current_sum = 0
+        result = float('inf')
+        for i in range(len(nums)):
+            current_sum += nums[i]
+            while current_sum >= target:
+                result = min(result, i - p1 + 1)
+                current_sum -= nums[p1]
+                p1 += 1
+        
+        return 0 if result == float('inf') else result
+
+
+if __name__ == "__main__":
+    target = 7
+    nums = [2,3,1,2,4,3]
+    result = Solution().minSubArrayLen(target, nums)
+    print(result)

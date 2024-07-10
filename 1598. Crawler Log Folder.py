6e2cb650ff16595d2aaf1ced3eb91ad551bc3c78
dogxxxxx <kaifin1001@gmail.com>
2024-07-10 02:50:55 -0700

    Add: 1598

---
+++ 1598. Crawler Log Folder.py
@@ -0,0 +1,27 @@
+from typing import List
+"""
+Method 1:
+Iterate the logs
+
+Time complexity: O(N)
+Space complexity: O(1)
+"""
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        result = 0
+        for step in logs:
+            if step == "./":
+                continue
+            elif step == "../":
+                result -= 1
+            else:
+                result += 1
+            result = max(result, 0)
+        return result
+    
+
+if __name__ == "__main__":
+    logs = ["d1/","d2/","../","d21/","./"]
+    res = Solution().minOperations(logs)
+    print(res)
